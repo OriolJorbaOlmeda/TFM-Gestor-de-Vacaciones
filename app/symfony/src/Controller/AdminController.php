dfe9e1acdfd809f15ad5f5d2b8ad1f38ab24b769
MireiaCastillejo <mireia.castillejo@students.salle.url.edu>
2022-07-24 23:57:24 +0200

    modificar usuario

--- app/symfony/src/Controller/AdminController.php
+++ app/symfony/src/Controller/AdminController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class AdminController extends AbstractController
 {
@@ -39,11 +40,65 @@ class AdminController extends AbstractController
             ]);
         }
     }
+
     #[Route('/admin/modificar_usuario', name: 'app_admin_modify-user')]
-    public function modifyUser(): Response
-    {
+    public function modifyUser(
+        Request $request,
+        UserRepository $userRepository,
+        Security $security,
+        DepartmentRepository $departmentRepository
+    ): Response {
+        ob_start();
+
+        $actualUser = $userRepository->findOneBy(array('email' => 'mireia16@hotmail.com'));
+        $departments = $departmentRepository->findBy(array('company' => '1'));
+        $selectedDepartment = $request->get('department');
+        print_r($selectedDepartment);
+        $users = $userRepository->findBy(array('department' => '2'));
+        if (isset($_POST['submit'])) {
+            if (isset($_POST['department']) && isset($_POST['user'])) {
+                $userid = $_POST['user'];
+                return $this->redirectToRoute('app_admin_edit-user', ['userid' => $userid]);
+            }
+        }
+
         return $this->render('admin/modificar_usuario.html.twig', [
             'controller_name' => 'AdminController',
+            'users' => $users,
+            'departments' => $departments
+
+        ]);
+    }
+
+    #[Route('/admin/modificar_usuario/{userid}', name: 'app_admin_edit-user')]
+    public function editUser(
+        string $userid,
+        Request $request,
+        UserRepository $userRepository,
+        Security $security,
+        DepartmentRepository $departmentRepository,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
+        $user = $userRepository->findOneBy(array('id' => $userid));
+        $form = $this->createForm(UserRegistrationType::class,$user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $pass = $form->get('password')->getData();
+            $user->setPassword($passwordHasher->hashPassword($user, $pass));
+            $pending_vacation_days = $form->get('total_vacation_days')->getData();
+            $roles = $form->get('roles')->getData();
+            $user->setRoles([$roles]);
+
+            $user->setPendingVacationDays($pending_vacation_days);
+            $userRepository->add($user, true);
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        return $this->render('admin/modificar_usuario.html.twig', [
+            'controller_name' => 'AdminController',
+            'user' => $user,
+            'form' => $form->createView()
 
         ]);
     }
