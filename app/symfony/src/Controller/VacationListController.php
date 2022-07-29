a4f0a04f2d43fc290ef6939519b62b78644d6c43
ArianeGutierrez <ariane.gutierrez@students.salle.url.edu>
2022-07-29 15:48:26 +0200

    Borrar petición

--- app/symfony/src/Controller/VacationListController.php
+++ app/symfony/src/Controller/VacationListController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\CalendarRepository;
 use App\Repository\PetitionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class VacationListController extends AbstractController
 {
-    public function __construct(private PetitionRepository $petitionRepository){}
+    public function __construct(
+        private PetitionRepository $petitionRepository,
+        private CalendarRepository $calendarRepository
+    ){}
 
     #[Route('/employee/vacation', name: 'app_employee_vacation')]
     public function vacation(): Response
     {
-        $calendar_id = "100"; // cambiar calendario
+        $calendar = $this->calendarRepository->findCurrentCalendar($this->getUser()->getDepartment()->getCompany());
+        $calendar_id = $calendar->getId();
 
         $vacations = $this->petitionRepository->findVacationsByUserAndCalendar($this->getUser()->getId(), $calendar_id);
 
@@ -25,6 +31,15 @@ class VacationListController extends AbstractController
             'vacations' => $vacations,
             'absences' => $absences
         ]);
+    }
 
+    #[Route('/employee/delete_petition', name: 'app_employee_delete_petition')]
+    public function deletePetition(Request $request): Response
+    {
+        $petitionId = $request->get('petitionId');
+        $petition = $this->petitionRepository->findOneBy(['id' => $petitionId]);
+        $this->petitionRepository->remove($petition, true);
+
+        return $this->redirectToRoute('app_employee_vacation');
     }
 }
--- app/symfony/src/Repository/CalendarRepository.php
+++ app/symfony/src/Repository/CalendarRepository.php
@@ -7,6 +7,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\Exception\ORMException;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\Persistence\ManagerRegistry;
+use DateTime;
 
 /**
  * @extends ServiceEntityRepository<Calendar>
@@ -45,6 +46,21 @@ class CalendarRepository extends ServiceEntityRepository
         if ($flush) {
             $this->_em->flush();
         }
+    }
+
+    public function findCurrentCalendar($company_id): ?Calendar {
+        $actual_date = new DateTime();
+        $actual_date = $actual_date->format('Y-m-d');
+
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.initial_date < :actual_date OR i.initial_date = :actual_date')
+            ->andWhere('i.final_date > :actual_date OR i.initial_date = :actual_date')
+            ->andWhere('i.company = :company_id')
+            ->setParameter('actual_date', $actual_date)
+            ->setParameter('company_id', $company_id)
+            ->getQuery()
+            ->getOneOrNullResult()
+            ;
     }
 
 //    /**
