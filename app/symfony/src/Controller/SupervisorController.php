d3cf0c9471a45f18d5503cc4d8a69c1c16ec2a03
MireiaCastillejo <mireia.castillejo@students.salle.url.edu>
2022-08-03 16:56:56 +0200

    download file

--- app/symfony/src/Controller/SupervisorController.php
+++ app/symfony/src/Controller/SupervisorController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Repository\PetitionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SupervisorController extends AbstractController
@@ -24,10 +26,18 @@ class SupervisorController extends AbstractController
 
         $petitions = $this->petitionRepository->findBy(['supervisor' => $this->getUser(), 'state' => 'PENDING']);
         $num_petitions = count($petitions);
+        $justify =[];
+        foreach ($petitions as $petition) {
+            if($petition->getJustify()) {
+                $justify [] = $petition->getJustify();
+            }
+
+        }
 
         return $this->render('supervisor/solicitudes_pendientes.html.twig', [
             'petitions' => $petitions,
-            'num_petitions' => $num_petitions
+            'num_petitions' => $num_petitions,
+            'justify'=>$justify
         ]);
     }
 
@@ -53,5 +63,16 @@ class SupervisorController extends AbstractController
         $this->petitionRepository->add($petition, true);
 
         return $this->redirectToRoute("app_supervisor_pending_requests");
+    }
+
+    /**
+     * @Route("/download/{filename}", name="download_file")
+     **/
+    public function downloadFileAction(string $filename,
+    ){
+        $destination = $this->getParameter('documents');
+        $response = new BinaryFileResponse($destination.'/'.$filename);
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT,$filename);
+        return $response;
     }
 }
