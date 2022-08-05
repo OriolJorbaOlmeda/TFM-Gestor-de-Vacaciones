cd2071058f79f245610cab5e5d28244c2b9d68a6
ArianeGutierrez <ariane.gutierrez@students.salle.url.edu>
2022-08-05 09:52:47 +0200

    Añadir el cambjo de pass sin acabar

--- app/symfony/src/Controller/DashboardController.php
+++ app/symfony/src/Controller/DashboardController.php
@@ -2,19 +2,17 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
-    private $security;
 
-    public function __construct(SecurityController $security)
-    {
-        $this->security = $security;
-    }
+    public function __construct(private SecurityController $security, private UserPasswordHasherInterface $passwordHasher) {}
 
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
@@ -29,13 +27,27 @@ class DashboardController extends AbstractController
             return $this->redirectToRoute('app_admin_dashboard');
     }
 
-    /**
-     * @Route("/change_locale/{locale}", name="change_locale")
-     */
+
+    #[Route('/change_locale/{locale}', name: 'change_locale')]
     public function changeLocale($locale, Request $request)
     {
         $request->getSession()->set('_locale', $locale);
 
         return $this->redirect($request->headers->get('referer'));
+    }
+
+    #[Route('/change_password', name: 'app_change_password')]
+    public function changePassword(): Response {
+
+        $pass = $this->getUser()->getPassword();
+
+
+
+        $form = $this->createForm(ChangePasswordType::class, ['pass' => $pass]);
+
+        return $this->render('/security/change_password.html.twig', [
+            'form' => $form->createView()
+        ]);
+
     }
 }
