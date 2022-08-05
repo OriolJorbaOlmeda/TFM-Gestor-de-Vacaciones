9bdcd786c8fe40eaf314d851abf6f215e5995eeb
ArianeGutierrez <ariane.gutierrez@students.salle.url.edu>
2022-08-05 12:44:49 +0200

    Mover el cambio de pass a SecurityController

--- app/symfony/src/Controller/DashboardController.php
+++ app/symfony/src/Controller/DashboardController.php
@@ -2,22 +2,16 @@
 
 namespace App\Controller;
 
-use App\Form\CalendarType;
-use App\Form\ChangePasswordType;
-use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
 
     public function __construct(
-        private SecurityController $security,
-        private UserPasswordHasherInterface $passwordHasher,
-        private UserRepository $userRepository) {}
+        private SecurityController $security) {}
 
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
@@ -41,26 +35,4 @@ class DashboardController extends AbstractController
         return $this->redirect($request->headers->get('referer'));
     }
 
-    #[Route('/change_password', name: 'app_change_password')]
-    public function changePassword(Request $request): Response {
-
-        $form = $this->createForm(ChangePasswordType::class, []);
-        $form->handleRequest($request);
-
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $newPass = $form->get('new_password')->getData();
-            $user = $this->userRepository->findOneBy(['id' => $this->getUser()->getId()]);
-            $user->setPassword($this->passwordHasher->hashPassword($user, $newPass));
-            $this->userRepository->add($user, true);
-
-            return $this->redirectToRoute('app_login');
-        }
-
-        return $this->render('/security/change_password.html.twig', [
-            'form' => $form->createView()
-        ]);
-
-    }
 }
--- app/symfony/src/Controller/SecurityController.php
+++ app/symfony/src/Controller/SecurityController.php
@@ -2,13 +2,23 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+
+    public function __construct(
+        private UserRepository $userRepository,
+        private UserPasswordHasherInterface $passwordHasher
+    ) {}
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -24,10 +34,37 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
+
+
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): Response
     {
         return $this->redirectToRoute('app_dashboard');
         //throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+    
+
+
+    #[Route('/change_password', name: 'app_change_password')]
+    public function changePassword(Request $request): Response {
+
+        $form = $this->createForm(ChangePasswordType::class, []);
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $newPass = $form->get('new_password')->getData();
+            $user = $this->userRepository->findOneBy(['id' => $this->getUser()->getId()]);
+            $user->setPassword($this->passwordHasher->hashPassword($user, $newPass));
+            $this->userRepository->add($user, true);
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('/security/change_password.html.twig', [
+            'form' => $form->createView()
+        ]);
+
     }
 }
