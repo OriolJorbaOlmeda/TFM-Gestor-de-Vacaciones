8b01e88c567e45cce575a9b95c2f78cf4c989ee3
ArianeGutierrez <ariane.gutierrez@students.salle.url.edu>
2022-08-10 14:00:00 +0200

    Crear CreateUserController

--- app/symfony/src/Controller/AdminController.php
+++ app/symfony/src/Controller/AdminController.php
@@ -25,45 +25,6 @@ class AdminController extends AbstractController
 
     ) {}
 
-    function randomPassword() {
-        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-        $pass = array(); //remember to declare $pass as an array
-        $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-        for ($i = 0; $i < 8; $i++) {
-            $n = rand(0, $alphaLength);
-            $pass[] = $alphabet[$n];
-        }
-        return implode($pass); //turn the array into a string
-    }
-
-    #[Route('/admin/crear_usuario', name: 'app_admin_create-user')]
-    public function createUser(Request $request, UserPasswordHasherInterface $passwordHasher): Response {
-
-        $user = new User();
-        $form = $this->createForm(UserRegistrationType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            //$pass = $form->get('password')->getData();
-            $password = $this->randomPassword();
-            $user->setPassword($passwordHasher->hashPassword($user, $password));
-            $user->setPendingVacationDays($user->getTotalVacationDays());
-
-            $roles = $form->get('roles')->getData();
-            $user->setRoles([$roles]);
-
-            $this->userRepository->add($user, true);
-
-            $this->dispatcher->dispatch(new UserRegistrationEvent($this->getUser()->getEmail(),$user->getEmail(), $user->getName(), $password ));
-            return $this->redirectToRoute('app_dashboard');
-        }
-
-        return $this->render('admin/crear_usuario.html.twig', [
-            "form" => $form->createView(),
-            "error" => $form->getErrors(),
-        ]);
-
-    }
 
 
     #[Route('/admin/modificar_usuario', name: 'app_admin_modify-user')]
