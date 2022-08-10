b6e79b9b3a0f80c0615110f1eec15da2620cf745
ArianeGutierrez <ariane.gutierrez@students.salle.url.edu>
2022-08-10 14:14:54 +0200

    Crear ModifyUserController

---
+++ app/symfony/src/Controller/User/ModifyUserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller\User;
+
+use App\Form\SelectUserType;
+use App\Form\UserModificationType;
+use App\Modules\User\Infrastucture\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ModifyUserController extends AbstractController
+{
+
+    public function __construct(
+        private UserRepository $userRepository
+
+    ) {}
+
+    #[Route('/admin/modificar_usuario', name: 'app_admin_modify-user')]
+    public function modifyUser(
+        Request $request,
+    ): Response {
+        $departments = $this->getUser()->getDepartment()->getCompany()->getDepartments();
+
+        $choices = [];
+        foreach ($departments as $choice) {
+            if(!str_contains(strtolower($choice->getName()),'admin')){
+                $choices[$choice->getName()] = $choice->getId();
+            }
+        }
+
+        $form = $this->createForm(SelectUserType::class, $choices);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //mirar si usuario no existe
+            $userid = $form->get('user')->getData();
+            return $this->redirectToRoute('app_admin_edit-user', ['userid' => $userid]);
+        }
+
+        return $this->render('admin/modificar_usuario.html.twig', [
+            'depar' => $form->createView(),
+            'departments' => $departments
+
+        ]);
+    }
+
+
+    #[Route('/admin/modificar_usuario/{userid}', name: 'app_admin_edit-user')]
+    public function editUser(string $userid, Request $request): Response
+    {
+        $user = $this->userRepository->findOneBy(array('id' => $userid));
+        $form = $this->createForm(UserModificationType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $roles = $form->get('roles')->getData();
+            $user->setRoles([$roles]);
+
+            $this->userRepository->add($user, true);
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        return $this->render('admin/modificar_usuario.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+
+        ]);
+    }
+
+}
