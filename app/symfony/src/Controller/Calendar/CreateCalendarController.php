a96dda979cc8ad3080d83629ec1ab081e98725ec
ArianeGutierrez <ariane.gutierrez@students.salle.url.edu>
2022-08-11 13:42:57 +0200

    CreateCalendarController and CreateCalendar caso de uso

---
+++ app/symfony/src/Controller/Calendar/CreateCalendarController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Calendar;
+
+use App\Entity\Calendar;
+use App\Modules\Calendar\Application\CreateCalendar;
+use App\Modules\Calendar\Infrastucture\Form\CalendarType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CreateCalendarController extends AbstractController
+{
+    public function __construct(private CreateCalendar $createCalendar){}
+
+    #[Route('/admin/create_calendar', name: 'app_create_calendar')]
+    public function createCalendar(Request $request): Response
+    {
+        $calendar = new Calendar();
+        $form = $this->createForm(CalendarType::class, $calendar);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->createCalendar->createCalendar($calendar);
+
+            return $this->redirectToRoute('app_create_festives', ['calendarId' => $calendar->getId()]);
+
+        }
+
+        return $this->render('admin/crear_calendario.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+
+
+}
