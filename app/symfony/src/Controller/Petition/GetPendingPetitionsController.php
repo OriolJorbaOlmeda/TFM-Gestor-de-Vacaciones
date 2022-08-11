8d44febead6d76f506bb48ff67c59d49a7eb1fc4
ArianeGutierrez <ariane.gutierrez@students.salle.url.edu>
2022-08-11 22:49:09 +0200

    Modificar justificantes de pendingPetitionsController

--- app/symfony/src/Controller/Petition/GetPendingPetitionsController.php
+++ app/symfony/src/Controller/Petition/GetPendingPetitionsController.php
@@ -9,31 +9,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class GetPendingPetitionsController extends AbstractController
 {
-    public function __construct(
-        private GetPendingPetitions $getPendingPetitions
-    ) {}
-
+    public function __construct(private GetPendingPetitions $getPendingPetitions) {}
 
     #[Route('/supervisor/pending_requests', name: 'app_supervisor_pending_requests')]
     public function pendingRequests(): Response
     {
-
         $petitions = $this->getPendingPetitions->getPendingPetitions();
-
         $num_petitions = count($petitions);
-
-        $justify =[];
-        foreach ($petitions as $petition) {
-            if($petition->getJustify()) {
-                $justify [] = $petition->getJustify();
-            }
-
-        }
 
         return $this->render('supervisor/solicitudes_pendientes.html.twig', [
             'petitions' => $petitions,
-            'num_petitions' => $num_petitions,
-            'justify'=>$justify
+            'num_petitions' => $num_petitions
         ]);
     }
 
