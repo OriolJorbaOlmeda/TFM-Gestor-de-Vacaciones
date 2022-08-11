d94a1220426b7c595c24f9a6d2180abb20a61e7c
ArianeGutierrez <ariane.gutierrez@students.salle.url.edu>
2022-08-11 16:37:58 +0200

    Update EmployeeHomeController.php

--- app/symfony/src/Controller/EmployeeHomeController.php
+++ app/symfony/src/Controller/EmployeeHomeController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class EmployeeController extends AbstractController
+class EmployeeHomeController extends AbstractController
 {
 
     public function __construct(
