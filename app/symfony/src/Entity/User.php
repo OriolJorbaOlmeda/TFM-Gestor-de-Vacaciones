723df3aa465fa9cd35a506f29eb90023a4990af1
OriolJorbaOlmeda <oriol.jorba.olmeda@gmail.com>
2022-08-22 18:46:19 +0200

    Basic unit test for all entities

--- app/symfony/src/Entity/User.php
+++ app/symfony/src/Entity/User.php
@@ -229,7 +229,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        $roles[] = 'ROLE_EMPLEADO';
 
         return array_unique($roles);
     }
